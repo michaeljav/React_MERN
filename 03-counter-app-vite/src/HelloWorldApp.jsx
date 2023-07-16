7a06a9bd5e0fb4657e05ee0f3fe7e3dee13d9c5a
Michael Javier Mota <michaeljaviermota@gmail.com>
2023-07-16 13:23:12 -0400

    first app 2

---
+++ 03-counter-app-vite/src/HelloWorldApp.jsx
@@ -0,0 +1,10 @@
+// export const App = () => {
+//   return <h1>Hello world22</h1>;
+// };
+
+const App = () => {
+  return <h1>Hello world22</h1>;
+};
+
+export const hola = '123';
+export default App;
