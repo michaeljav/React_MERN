ec9c7bdcda54de98e30f9a4554aeb8b743e50773
Michael Javier Mota <michaeljaviermota@gmail.com>
2023-07-16 13:09:27 -0400

    First App REACT + VITE

--- 03-counter-app-vite/src/main.jsx
+++ 03-counter-app-vite/src/main.jsx
@@ -1,10 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 
+// const App = () => {
+//   return <h1>Hello world</h1>;
+// };
+
+function App() {
+  return <h1>Hello world</h1>;
+}
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
