127ce3220067bca301616a12734d613cf3e5a58e
Michael Javier Mota <michaeljaviermota@gmail.com>
2023-07-17 15:10:19 -0400

    JEST FETCH TEST

---
+++ 03-counter-app-vite/tests/base-pruebas/09-promesas.test.js
@@ -0,0 +1,38 @@
+import { getHeroeByIdAsync } from '../../src/base-pruebas/09-promesas';
+
+describe('Pruebas en 09-promesas', () => {
+  test('getHeroeByIdAsync debe de retornar un heroe', (done) => {
+    const id = 1;
+    const result = {
+      id: 1,
+      name: 'Batman',
+      owner: 'DC',
+    };
+    getHeroeByIdAsync(id).then((hero) => {
+      console.log(hero);
+      expect(hero).toEqual(result);
+      // expect(true).toBe(false);
+      //to wait to finish
+      done();
+    });
+  });
+
+  test('getHeroeByIdAsync debe obtener un error si no existe', (done) => {
+    const id = 100;
+    const result = {
+      id: 1,
+      name: 'Batman',
+      owner: 'DC',
+    };
+    getHeroeByIdAsync(id)
+      .then((hero) => {
+        expect(hero).toBeFalsy();
+        done();
+      })
+      .catch((err) => {
+        // console.log(err);
+        expect(err).toBe(`No se pudo encontrar el héroe ${id}`);
+        done();
+      });
+  });
+});
