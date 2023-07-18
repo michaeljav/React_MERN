aa17a9b5e0239784c912616fde2b663658e440dc
Michael Javier Mota <michaeljaviermota@gmail.com>
2023-07-17 20:18:21 -0400

    Primera prueba de container con snapshot

--- 03-counter-app-vite/tests/FirstApp.test.jsx
+++ 03-counter-app-vite/tests/FirstApp.test.jsx
@@ -4,6 +4,20 @@ import { FirstApp } from '../src/FirstApp';
 describe('Pruebas en <FirstApp />', () => {
   test('debe de hacer match con el snapshot', () => {
     const title = 'Holam michael';
-    render(<FirstApp title={title} />);
+    const { container } = render(<FirstApp title={title} />);
+
+    expect(container).toMatchSnapshot();
+  });
+
+  test('debe de mostrar el Título en h1', () => {
+    const title = 'Hola michael';
+    const { container, getByText } = render(<FirstApp title={title} />);
+
+    expect(getByText(title)).toBeTruthy();
+
+    const h1 = container.querySelector('h1');
+
+    expect(h1.innerHTML).toBe(title);
+    expect(h1.innerHTML).toContain(title);
   });
 });
