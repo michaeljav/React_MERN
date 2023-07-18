61dc88e6933ce09bcafdd2ddfc9f1812a8bf14a1
Michael Javier Mota <michaeljaviermota@gmail.com>
2023-07-17 20:39:22 -0400

    validandoc elementes testing

--- 03-counter-app-vite/tests/FirstApp.test.jsx
+++ 03-counter-app-vite/tests/FirstApp.test.jsx
@@ -2,22 +2,43 @@ import { render } from '@testing-library/react';
 import { FirstApp } from '../src/FirstApp';
 
 describe('Pruebas en <FirstApp />', () => {
-  test('debe de hacer match con el snapshot', () => {
-    const title = 'Holam michael';
-    const { container } = render(<FirstApp title={title} />);
+  // test('debe de hacer match con el snapshot', () => {
+  //   const title = 'Holam michael';
+  //   const { container } = render(<FirstApp title={title} />);
 
-    expect(container).toMatchSnapshot();
-  });
+  //   expect(container).toMatchSnapshot();
+  // });
 
   test('debe de mostrar el Título en h1', () => {
     const title = 'Hola michael';
-    const { container, getByText } = render(<FirstApp title={title} />);
+    const { container, getByText, getByTestId } = render(
+      <FirstApp title={title} />
+    );
 
     expect(getByText(title)).toBeTruthy();
 
-    const h1 = container.querySelector('h1');
+    // const h1 = container.querySelector('h1');
+    // expect(h1.innerHTML).toBe(title);
+    // expect(h1.innerHTML).toContain(title);
 
-    expect(h1.innerHTML).toBe(title);
-    expect(h1.innerHTML).toContain(title);
+    //que exista
+    // expect(getByTestId('test-title')).toBeTruthy();
+    // //verificar contenido
+    expect(getByTestId('test-title').innerHTML).toBe(title);
+    // //que contenga el titutulo
+    expect(getByTestId('test-title').innerHTML).toContain(title);
+  });
+
+  test('debe de mostrar el subtitulo enviado por props', () => {
+    const title = 'Hola michael';
+    const subTitle = 'Soy un subtitulo';
+    const { getByText, getAllByText } = render(
+      <FirstApp title={title} subTitle={subTitle} />
+    );
+
+    //verificar que exista un subtitulo
+    // expect(getByText(subTitle)).toBeTruthy();
+    //si aparecen mas de uno
+    expect(getAllByText(subTitle).length).toBe(2);
   });
 });
