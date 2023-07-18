7b3b369e03fc0e00d028378646c2974cd9387611
Michael Javier Mota <michaeljaviermota@gmail.com>
2023-07-18 09:07:05 -0400

    adde Map list

--- 04-gif-expert-app/src/GifExpertApp.jsx
+++ 04-gif-expert-app/src/GifExpertApp.jsx
@@ -1,11 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export const GifExpertApp = () => {
+  const [categories, setCategories] = useState(['One Punch', 'Dragon Ball']);
+
   return (
     <>
       {/* TITULO */}
       <h1>GifExpertApp</h1>;{/* INPUT */}
       {/* LISTADO DE GIF */}
+      <ol>
+        {categories.map((category) => {
+          return <li key={category}>{category}</li>;
+        })}
+      </ol>
       {/* GIF ITEM */}
     </>
   );
