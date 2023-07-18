aa8d7733f8445f15106a7a806caebf46685e83d8
Michael Javier Mota <michaeljaviermota@gmail.com>
2023-07-18 09:12:07 -0400

    add element to list

--- 04-gif-expert-app/src/GifExpertApp.jsx
+++ 04-gif-expert-app/src/GifExpertApp.jsx
@@ -3,11 +3,15 @@ import React, { useState } from 'react';
 export const GifExpertApp = () => {
   const [categories, setCategories] = useState(['One Punch', 'Dragon Ball']);
 
+  const onAddCategory = () => {
+    setCategories((prev) => [...prev, 'JAVIERa']);
+  };
   return (
     <>
       {/* TITULO */}
       <h1>GifExpertApp</h1>;{/* INPUT */}
       {/* LISTADO DE GIF */}
+      <button onClick={onAddCategory}>Agregar</button>
       <ol>
         {categories.map((category) => {
           return <li key={category}>{category}</li>;
