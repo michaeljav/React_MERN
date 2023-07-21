f5b91b25e76815b285ef8b66dc9405aa48e810ee
Michael Javier Mota <michaeljaviermota@gmail.com>
2023-07-21 17:08:05 -0400

    Probando primer componete

--- 04-gif-expert-app/tests/components/GifItems.test.jsx
+++ 04-gif-expert-app/tests/components/GifItems.test.jsx
@@ -1,4 +1,4 @@
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import { GifItem } from '../../src/components/GifItem';
 
 describe('<Pruebas en <GifItem />', () => {
@@ -7,5 +7,21 @@ describe('<Pruebas en <GifItem />', () => {
   test('debe de hacer match con el snapshot', () => {
     const { container } = render(<GifItem title={title} url={url} />);
     expect(container).toMatchSnapshot();
+  });
+
+  test('debe de mostrar la imagen con la url y el Alt indicado', () => {
+    render(<GifItem title={title} url={url} />);
+    // screen.debug();
+    //One opction
+    expect(screen.getByRole('img').src).toBe(url);
+    //second option
+    const { src, alt } = screen.getByRole('img');
+    expect(src).toBe(src);
+    expect(alt).toBe(alt);
+  });
+
+  test('debe de mostrar el titulo del componente', () => {
+    render(<GifItem title={title} url={url} />);
+    expect(screen.getByText(title)).toBeTruthy();
   });
 });
