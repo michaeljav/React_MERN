f9feb0ceeb6436aff142ea9b1ee45dbef23952f2
Michael Javier Mota <michaeljaviermota@gmail.com>
2023-07-21 17:46:18 -0400

    añadido evento a input al componente addcategory

--- 04-gif-expert-app/src/components/AddCategory.jsx
+++ 04-gif-expert-app/src/components/AddCategory.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { PropTypes } from 'prop-types';
 
 export const AddCategory = ({ onNewCategory }) => {
   const [inputValue, setInputValue] = useState('');
@@ -24,4 +25,7 @@ export const AddCategory = ({ onNewCategory }) => {
       />
     </form>
   );
+};
+AddCategory.propTypes = {
+  onNewCategory: PropTypes.func.isRequered,
 };
---
+++ 04-gif-expert-app/tests/components/AddCategory.test.jsx
@@ -0,0 +1,14 @@
+import { fireEvent, render, screen } from '@testing-library/react';
+import { AddCategory } from '../../src/components/AddCategory';
+
+describe('Pruebas en <AddCategory/>', () => {
+  test('debe de cambiar el valor de la caja de texto', () => {
+    //sugete de prueba
+    render(<AddCategory onNewCategory={() => {}} />);
+    const input = screen.getByRole('textbox');
+
+    fireEvent.input(input, { target: { value: 'Saitama' } });
+    expect(input.value).toBe('Saitama');
+    screen.debug();
+  });
+});
