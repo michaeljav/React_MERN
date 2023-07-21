2c47cef1574774622b2ed9bc37bca91f4fddbaef
Michael Javier Mota <michaeljaviermota@gmail.com>
2023-07-20 20:23:42 -0400

    short if isloading waiting

--- 04-gif-expert-app/src/components/GitGrid.jsx
+++ 04-gif-expert-app/src/components/GitGrid.jsx
@@ -10,6 +10,7 @@ export const GitGrid = ({ category }) => {
   return (
     <>
       <h3>{category}</h3>
+      {isLoading && <h2>Cargando...</h2>}
       <div className='card-grid'>
         {images.map((image) => (
           <GifItem key={image.id} {...image} />
