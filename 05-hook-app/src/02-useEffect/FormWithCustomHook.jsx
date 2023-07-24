f59824b69654f45ac357fe41f098b7769f7f3620
Michael Javier Mota <michaeljaviermota@gmail.com>
2023-07-24 19:09:30 -0400

    crear un custom hook para recolectar datos del formulario

---
+++ 05-hook-app/src/02-useEffect/FormWithCustomHook.jsx
@@ -0,0 +1,44 @@
+import React, { useEffect, useState } from 'react';
+import { Message } from './Message';
+import { useForm } from '../hooks/useForm';
+
+export const FormWithCustomHook = () => {
+  const { formState, onInputChange, username, email, password } = useForm({
+    username: '',
+    email: '',
+    password: '',
+  });
+  // const { username, email, password } = formState;
+
+  return (
+    <>
+      <h1>Formulario con custom hook</h1>
+      <hr />
+      <input
+        type='text'
+        className='form-control'
+        placeholder='Username'
+        name='username'
+        value={username}
+        onChange={onInputChange}
+      />
+      {username === 'michael1' && <Message />}
+      <input
+        type='email'
+        className='form-control mt-2'
+        placeholder='michaeljm@gmail.com'
+        name='email'
+        value={email}
+        onChange={onInputChange}
+      />
+      <input
+        type='password'
+        className='form-control mt-2'
+        placeholder='constraseña'
+        name='password'
+        value={password}
+        onChange={onInputChange}
+      />
+    </>
+  );
+};
--- 05-hook-app/src/main.jsx
+++ 05-hook-app/src/main.jsx
@@ -5,10 +5,12 @@ import './index.css';
 // import { CounterApp } from './01-useState/CounterApp';
 // import { CounterWithCustomHook } from './01-useState/CounterWithCustomHook';
 import { SimpleForms } from './02-useEffect/SimpleForms';
+import { FormWithCustomHook } from './02-useEffect/FormWithCustomHook';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <SimpleForms />
+    <FormWithCustomHook />
+    {/* <SimpleForms /> */}
     {/* <CounterWithCustomHook /> */}
     {/* <HooksApp /> */}
     {/* <CounterApp /> */}
