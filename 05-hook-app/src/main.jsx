6534546c9846be4af1406c96de00dc268696b0ac
Michael Javier Mota <michaeljaviermota@gmail.com>
2023-07-25 17:28:56 -0400

    Tarea memo

--- 05-hook-app/src/main.jsx
+++ 05-hook-app/src/main.jsx
@@ -12,12 +12,14 @@ import './index.css';
 // import { Memorize } from './06-memos/Memorize';
 // import { MemoHook } from './06-memos/MemoHook';
 import { CallbackHook } from './06-memos/CallbackHook';
+import { Padre } from './07-tarea-memo/Padre';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <>
     {/* <React.StrictMode> */}
 
-    <CallbackHook />
+    <Padre />
+    {/* <CallbackHook /> */}
     {/* <MemoHook /> */}
     {/* <Memorize/> */}
     {/* <Layout /> */}
