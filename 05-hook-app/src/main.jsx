93312565a625ebd6ed087b9e5fbfdad21fdb89c8
Michael Javier Mota <michaeljaviermota@gmail.com>
2023-07-25 14:47:18 -0400

    memo

--- 05-hook-app/src/main.jsx
+++ 05-hook-app/src/main.jsx
@@ -9,10 +9,12 @@ import { FormWithCustomHook } from './02-useEffect/FormWithCustomHook';
 import { MultipleCustomHooks } from './03-examples/MultipleCustomHooks';
 import { FocusScreen } from './04-useRef/FocusScreen';
 import { Layout } from './05-useLayoutEffect/Layout';
+import { Memorize } from './06-memos/Memorize';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Layout />
+    <Memorize></Memorize>
+    {/* <Layout /> */}
     {/* <FocusScreen /> */}
     {/* <MultipleCustomHooks /> */}
     {/* <FormWithCustomHook /> */}
