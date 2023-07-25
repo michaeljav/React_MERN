8e853d7883557094bd15948b5d8cd682f4f59539
Michael Javier Mota <michaeljaviermota@gmail.com>
2023-07-25 18:53:51 -0400

    como funciona el reducer

---
+++ 05-hook-app/src/08-useReducer/intro-reducer.js
@@ -0,0 +1,36 @@
+const initialState = [
+  {
+    id: 1,
+    todo: 'Recolecatr las piedra del Alma',
+    done: false,
+  },
+];
+
+//funcion que cambia el estado
+const todoReducer = (state = initialState, action = {}) => {
+  switch (action.type) {
+    case '[TODO] add todo':
+      return [...state, action.payload];
+    default:
+      return state;
+  }
+};
+
+//nevo
+let todos = todoReducer();
+
+const newTodo = {
+  id: 2,
+  todo: 'Recolecatr las piedra del poder',
+  done: false,
+};
+
+//actions
+const addTodoAction = {
+  type: '[TODO] add todo',
+  payload: newTodo,
+};
+
+todos = todoReducer(todos, addTodoAction);
+
+console.log(todos);
--- 05-hook-app/src/main.jsx
+++ 05-hook-app/src/main.jsx
@@ -11,25 +11,27 @@ import './index.css';
 // import { Layout } from './05-useLayoutEffect/Layout';
 // import { Memorize } from './06-memos/Memorize';
 // import { MemoHook } from './06-memos/MemoHook';
-import { CallbackHook } from './06-memos/CallbackHook';
+// import { CallbackHook } from './06-memos/CallbackHook';
 import { Padre } from './07-tarea-memo/Padre';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <>
-    {/* <React.StrictMode> */}
+import './08-useReducer/intro-reducer';
 
-    <Padre />
-    {/* <CallbackHook /> */}
-    {/* <MemoHook /> */}
-    {/* <Memorize/> */}
-    {/* <Layout /> */}
-    {/* <FocusScreen /> */}
-    {/* <MultipleCustomHooks /> */}
-    {/* <FormWithCustomHook /> */}
-    {/* <SimpleForms /> */}
-    {/* <CounterWithCustomHook /> */}
-    {/* <HooksApp /> */}
-    {/* <CounterApp /> */}
-    {/* </React.StrictMode> */}
-  </>
-);
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//   <>
+//     {/* <React.StrictMode> */}
+
+//     <Padre />
+//     {/* <CallbackHook /> */}
+//     {/* <MemoHook /> */}
+//     {/* <Memorize/> */}
+//     {/* <Layout /> */}
+//     {/* <FocusScreen /> */}
+//     {/* <MultipleCustomHooks /> */}
+//     {/* <FormWithCustomHook /> */}
+//     {/* <SimpleForms /> */}
+//     {/* <CounterWithCustomHook /> */}
+//     {/* <HooksApp /> */}
+//     {/* <CounterApp /> */}
+//     {/* </React.StrictMode> */}
+//   </>
+// );
