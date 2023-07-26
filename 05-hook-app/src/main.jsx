2cab886c13403b9429c586c239ec6e22167e5556
Michael Javier Mota <michaeljaviermota@gmail.com>
2023-07-26 13:08:47 -0400

    Primera configuracion para context y router

--- 05-hook-app/src/main.jsx
+++ 05-hook-app/src/main.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 // import { HooksApp } from './HooksApp';
 // import { CounterApp } from './01-useState/CounterApp';
@@ -12,28 +13,16 @@ import './index.css';
 // import { Memorize } from './06-memos/Memorize';
 // import { MemoHook } from './06-memos/MemoHook';
 // import { CallbackHook } from './06-memos/CallbackHook';
-import { Padre } from './07-tarea-memo/Padre';
+// import { Padre } from './07-tarea-memo/Padre';
 
 // import './08-useReducer/intro-reducer';
-import { TodoApp } from './08-useReducer/TodoApp';
+// import { TodoApp } from './08-useReducer/TodoApp';
+import { MainApp } from './09-useContext/MainApp';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <>
+  <BrowserRouter>
     {/* <React.StrictMode> */}
-
-    <TodoApp />
-    {/* <Padre /> */}
-    {/* <CallbackHook /> */}
-    {/* <MemoHook /> */}
-    {/* <Memorize/> */}
-    {/* <Layout /> */}
-    {/* <FocusScreen /> */}
-    {/* <MultipleCustomHooks /> */}
-    {/* <FormWithCustomHook /> */}
-    {/* <SimpleForms /> */}
-    {/* <CounterWithCustomHook /> */}
-    {/* <HooksApp /> */}
-    {/* <CounterApp /> */}
+    <MainApp />
     {/* </React.StrictMode> */}
-  </>
+  </BrowserRouter>
 );
