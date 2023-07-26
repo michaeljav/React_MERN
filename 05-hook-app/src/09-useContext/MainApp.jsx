cf2f20a5022fd58a3a4f0fc7e236b4c8ece5fcac
Michael Javier Mota <michaeljaviermota@gmail.com>
2023-07-26 13:38:17 -0400

    Implementado route

--- 05-hook-app/src/09-useContext/MainApp.jsx
+++ 05-hook-app/src/09-useContext/MainApp.jsx
@@ -1,10 +1,24 @@
 import React from 'react';
+import { Navigate, Route, Routes } from 'react-router-dom';
+import { HomePage } from './HomePage';
+import { LoginPage } from './LoginPage';
+import { AboutPage } from './AboutPage';
 
 export const MainApp = () => {
   return (
     <>
       <h1>MainApp</h1>
       <hr />
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route path='/login' element={<LoginPage />} />
+        <Route path='/about' element={<AboutPage />} />
+        {/* Si no existe la ruta AUNQU SE QUEDA EN LA RUTA EN LA URL */}
+        {/* <Route path='/*' element={<LoginPage />} /> */}
+
+        {/* ESTA ME MUEVE A LA RUTA */}
+        <Route path='/*' element={<Navigate to='/' />} />
+      </Routes>
     </>
   );
 };
