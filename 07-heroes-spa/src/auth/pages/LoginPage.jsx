e64668e1867961a5acacef08f8904c4c652ed70e
Michael Javier Mota <michaeljaviermota@gmail.com>
2023-08-03 16:08:33 -0400

    dos routers navegar login logout menu ir atras pagina

--- 07-heroes-spa/src/auth/pages/LoginPage.jsx
+++ 07-heroes-spa/src/auth/pages/LoginPage.jsx
@@ -1,8 +1,20 @@
+import { useNavigate } from 'react-router-dom';
+
 export const LoginPage = () => {
+  const navigate = useNavigate();
+  const onLogin = () => {
+    //
+    navigate('/', { replace: true });
+  };
   return (
-    <>
-      <p>LoginPAGE</p>
-    </>
+    <div className='cointainer mt-5'>
+      <h1>Login</h1>
+      <hr />
+
+      <button onClick={onLogin} className='btn btn-primary'>
+        Login
+      </button>
+    </div>
   );
 };
 
