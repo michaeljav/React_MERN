23b3b834978a402620c33facca3c7424e5b87619
Michael Javier Mota <michaeljaviermota@gmail.com>
2023-08-04 15:59:35 -0400

    finalizado 214  video router login no acceder sin permiso

--- 07-heroes-spa/src/router/AppRouter.jsx
+++ 07-heroes-spa/src/router/AppRouter.jsx
@@ -2,13 +2,24 @@ import { Navigate, Route, Routes } from 'react-router-dom';
 
 import { LoginPage } from '../auth';
 import { HeroesRoutes } from '../heroes';
+import { PrivateRoute } from './PrivateRoute';
 
 export const AppRouter = () => {
   return (
     <>
       <Routes>
         <Route path='login' element={<LoginPage />} />
-        <Route path='/*' element={<HeroesRoutes />} />
+
+        {/* para proteger esta ruta que debe de ser vista solo si  estoy loggiado */}
+        <Route
+          path='/*'
+          element={
+            <PrivateRoute>
+              <HeroesRoutes />
+            </PrivateRoute>
+          }
+        />
+        {/* <Route path='/*' element={<HeroesRoutes />} /> */}
       </Routes>
     </>
   );
