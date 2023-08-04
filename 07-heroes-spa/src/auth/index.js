40afde08d65c9eb30e3c1a185750859c4dcd03e4
Michael Javier Mota <michaeljaviermota@gmail.com>
2023-08-04 12:18:10 -0400

    Section 15 Rutas privadas y pubagregado context

--- 07-heroes-spa/src/auth/index.js
+++ 07-heroes-spa/src/auth/index.js
@@ -1,5 +1,2 @@
-// export * from './components';
-// export * from './hooks';
-// export * from '../auth/components';
-// export * from '../auth/hooks';
-export * from '../auth/pages';
+export * from './context';
+export * from './pages';
