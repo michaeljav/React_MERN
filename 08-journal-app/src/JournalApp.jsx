a9a8e47487e395aeb6e77068b3d8747636ede356
Michael Javier Mota <michaeljaviermota@gmail.com>
2023-08-07 22:18:04 -0400

    241  instando material ui 2 finalizada instalacion

--- 08-journal-app/src/JournalApp.jsx
+++ 08-journal-app/src/JournalApp.jsx
@@ -1,11 +1,11 @@
-import React from 'react';
 import { AppRouter } from './router/AppRouter';
+import { AppTheme } from './theme';
 
 export const JournalApp = () => {
   return (
-    <>
+    <AppTheme>
       {/* <h1>Hola Mundo Journal App</h1> */}
       <AppRouter />
-    </>
+    </AppTheme>
   );
 };
