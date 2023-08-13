30d735804b6444df4339b8e984c07232490f8047
Michael Javier Mota <michaeljaviermota@gmail.com>
2023-08-13 16:19:39 -0400

    Template web galery image navbar sidebar

--- 08-journal-app/src/auth/layout/JournalLayout.jsx
+++ 08-journal-app/src/auth/layout/JournalLayout.jsx
@@ -1,6 +1,6 @@
 import { Box, Toolbar } from '@mui/material';
 
-import { NavBar, SideBar } from './components';
+import { NavBar, SideBar } from '../../journal/components';
 
 /** */
 
@@ -13,7 +13,7 @@ export const JournalLayout = ({ children }) => {
       {/* Sidebar drawerWith */}
       <SideBar drawerWith={drawerWith} />
       {/* En ves de un div que sea la etiqueta  main */}
-      <Box component='main' sx={{ flexGrowl: 1, p: 3 }}>
+      <Box component='main' sx={{ flexGrow: 1, p: 3 }}>
         {/* Toolbar */}
         <Toolbar />
         {children}
--- 08-journal-app/src/journal/pages/JournalPage.jsx
+++ 08-journal-app/src/journal/pages/JournalPage.jsx
@@ -1,17 +1,36 @@
-import { MailOutline } from '@mui/icons-material';
-import { Typography } from '@mui/material';
+import { AddOutlined, MailOutline } from '@mui/icons-material';
+import { IconButton, Typography } from '@mui/material';
 import React from 'react';
 import { JournalLayout } from '../../auth/layout/JournalLayout';
+import { NothingSelectedView } from '../views/NothingSelectedView';
+import { NoteView } from '../views/NoteView';
 
 export const JournalPage = () => {
   return (
     <JournalLayout>
-      <Typography>
+      {/* <Typography>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus assumenda
         vel quo corrupti ducimus, repellendus qui eos quasi natus perferendis
         saepe debitis ad maxime facere totam ab numquam labore possimus.
-      </Typography>
+      </Typography> */}
       {/* Nothingselected */}
+      {/* <NothingSelectedView /> */}
+      <NoteView />
+
+      <IconButton
+        size='large'
+        sx={{
+          color: 'white',
+          backgroundColor: 'error.main',
+          ':hover': { backgroundColor: 'error.main', opacity: 0.9 },
+          position: 'fixed',
+          right: 50,
+          bottom: 50,
+        }}
+      >
+        <AddOutlined sx={{ fontSize: 30 }} />
+      </IconButton>
+
       {/* noteview */}
     </JournalLayout>
   );
