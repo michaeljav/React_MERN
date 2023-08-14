ac3f92911395cfb7bd2fbeb8e9ed223c4fefb6f0
Michael Javier Mota <michaeljaviermota@gmail.com>
2023-08-13 20:15:30 -0400

    Redux toolkit  async thunk  axios

---
+++ 09-toolkit-redux/src/store/slices/pokemon/thunks.js
@@ -0,0 +1,20 @@
+import { pokemonApi } from '../../../api/pockemonApi';
+import { setPokemon, startLoadingPokemon } from './pokemonSlice';
+
+export const getPokemons = (page = 0) => {
+  return async (dispatch, getState) => {
+    dispatch(startLoadingPokemon());
+
+    //TODO: realizar peticion http
+    // const resp = await fetch(
+    //   `https://pokeapi.co/api/v2/pokemon?limit=10&offset=${page * 10}`
+    // );
+    // const data = await resp.json();
+
+    const { data } = await pokemonApi.get(
+      `/pokemon?limit=10&offset=${page * 10}`
+    );
+
+    dispatch(setPokemon({ pokemons: data.results, page: page + 1 }));
+  };
+};
--- 09-toolkit-redux/src/store/store.js
+++ 09-toolkit-redux/src/store/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { counterSlice } from './slices/counter';
+import { pokemonSlice } from './slices/pokemon';
 
 // step 1
 export const store = configureStore({
   reducer: {
     counter: counterSlice.reducer,
+    pokemon: pokemonSlice.reducer,
   },
 });
