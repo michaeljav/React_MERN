25d4edb9b0e5757077dd158914178840b6fc322b
Michael Javier Mota <michaeljaviermota@gmail.com>
2023-08-25 14:35:04 -0400

    event  localstorage para en refresh siga en el tab

--- 10-calendar/src/calendar/pages/CalendarPage.jsx
+++ 10-calendar/src/calendar/pages/CalendarPage.jsx
@@ -1,26 +1,10 @@
-import { Calendar, dateFnsLocalizer } from 'react-big-calendar';
+import { Calendar } from 'react-big-calendar';
 import 'react-big-calendar/lib/css/react-big-calendar.css';
 
-// import format from 'date-fns/format'
-// import parse from 'date-fns/parse'
-// import startOfWeek from 'date-fns/startOfWeek'
-// import getDay from 'date-fns/getDay'
-import enUS from 'date-fns/locale/en-US';
-
-import { Navbar } from '../';
-import { addHours, format, parse, startOfWeek, getDay } from 'date-fns';
-
-const locales = {
-  'en-US': enUS,
-};
-
-const localizer = dateFnsLocalizer({
-  format,
-  parse,
-  startOfWeek,
-  getDay,
-  locales,
-});
+import { addHours } from 'date-fns';
+import { CalendarEvent, Navbar } from '../';
+import { getMessagesES, localizer } from '../../helpers';
+import { useState } from 'react';
 
 const events = [
   {
@@ -37,16 +21,55 @@ const events = [
 ];
 
 export const CalendarPage = () => {
+  const [lastView, setLastView] = useState(
+    localStorage.getItem('lastView') || 'week'
+  );
+
+  const eventStyleGetter = (event, start, end, isSelected) => {
+    // console.log({ event, start, end, isSelected });
+
+    const style = {
+      backgroundColor: '#347CF7',
+      borderRadius: '0px',
+      opacity: 0.8,
+      color: 'white',
+    };
+
+    return { style: style };
+  };
+
+  const onDoubleClick = (event) => {
+    console.log({ doubleClick: event });
+  };
+
+  const onSelect = (event) => {
+    console.log({ click: event });
+  };
+
+  const onViewChanged = (event) => {
+    localStorage.setItem('lastView', event);
+  };
+
   return (
     <>
       <Navbar />
 
       <Calendar
+        culture='es'
         localizer={localizer}
         events={events}
+        defaultView={lastView}
         startAccessor='start'
         endAccessor='end'
         style={{ height: 'calc(100vh - 80px' }}
+        messages={getMessagesES()}
+        eventPropGetter={eventStyleGetter}
+        components={{
+          event: CalendarEvent,
+        }}
+        onDoubleClickEvent={onDoubleClick}
+        onSelectEvent={onSelect}
+        onView={onViewChanged}
       />
     </>
   );
---
+++ 10-calendar/src/helpers/getMessages.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+export const getMessagesES = () => {
+  return {
+    allDay: 'Todo el día',
+    previous: '<',
+    next: '>',
+    today: 'Hoy',
+    month: 'Mes',
+    week: 'Semana',
+    day: 'Día',
+    agenda: 'Agenda',
+    date: 'Fecha',
+    time: 'Hora',
+    event: 'Evento',
+    noEventsInRange: 'No hay eventos en este rango',
+    showMore: (total) => `+ Ver más (${total})`,
+  };
+};
