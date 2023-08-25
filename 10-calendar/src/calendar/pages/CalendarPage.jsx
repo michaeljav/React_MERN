336fb5ec25f41f74deb6b29c7b206b1001d2228c
Michael Javier Mota <michaeljaviermota@gmail.com>
2023-08-25 13:46:49 -0400

    Mostrando calendario canlendar

--- 10-calendar/src/calendar/pages/CalendarPage.jsx
+++ 10-calendar/src/calendar/pages/CalendarPage.jsx
@@ -1,9 +1,53 @@
+import { Calendar, dateFnsLocalizer } from 'react-big-calendar';
+import 'react-big-calendar/lib/css/react-big-calendar.css';
+
+// import format from 'date-fns/format'
+// import parse from 'date-fns/parse'
+// import startOfWeek from 'date-fns/startOfWeek'
+// import getDay from 'date-fns/getDay'
+import enUS from 'date-fns/locale/en-US';
+
 import { Navbar } from '../';
+import { addHours, format, parse, startOfWeek, getDay } from 'date-fns';
+
+const locales = {
+  'en-US': enUS,
+};
+
+const localizer = dateFnsLocalizer({
+  format,
+  parse,
+  startOfWeek,
+  getDay,
+  locales,
+});
+
+const events = [
+  {
+    title: 'Cumpleanos del jefe',
+    notes: 'comprar pastel',
+    start: new Date(),
+    end: addHours(new Date(), 2),
+    bgColor: '#fafafa',
+    user: {
+      _id: '123',
+      name: 'Michael',
+    },
+  },
+];
 
 export const CalendarPage = () => {
   return (
     <>
       <Navbar />
+
+      <Calendar
+        localizer={localizer}
+        events={events}
+        startAccessor='start'
+        endAccessor='end'
+        style={{ height: 'calc(100vh - 80px' }}
+      />
     </>
   );
 };
