bbfe5bd18e9786bd6b3dbd824713437d75d79ec1
Michael Javier Mota <michaeljaviermota@gmail.com>
2023-08-29 10:59:49 -0400

    routes backend

--- 10-calendar-backend/index.js
+++ 10-calendar-backend/index.js
@@ -1,1 +1,20 @@
-console.log('hola mundo!');
+const express = require('express');
+require('dotenv').config();
+
+console.log(process.env);
+//create server express
+const app = express();
+
+//Directorio Publico
+app.use(express.static('public'));
+
+//Routers
+app.use('/api/v1/auth', require('./routes/auth'));
+
+//TODO:  auth // create, login, renew
+//TODO
+
+//listen request
+app.listen(process.env.PORT, (req, res) => {
+  console.log(`Server running en port ${process.env.PORT}`);
+});
---
+++ 10-calendar-backend/routes/auth.js
@@ -0,0 +1,17 @@
+/**\
+ *
+ * User auth routes
+ * host + api/auth
+ */
+
+const { Router } = require('express');
+
+const router = Router();
+
+router.get('/', (req, res) => {
+  res.json({
+    ok: true,
+  });
+});
+
+module.exports = router;
