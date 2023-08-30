ec8b538d0924db45e40ef619df003fbdcc49c176
Michael Javier Mota <michaeljaviermota@gmail.com>
2023-08-30 10:12:13 -0400

    first save in mongodb  with mongoose node

--- 10-calendar-backend/controllers/auth.js
+++ 10-calendar-backend/controllers/auth.js
@@ -1,15 +1,25 @@
 const express = require('express');
+const User = require('../models/user.model');
 
-const createUser = (req, res = express.reponse) => {
+const createUser = async (req, res = express.reponse) => {
   const { name, email, password } = req.body;
+  try {
+    //save in database
+    const user = new User(req.body);
 
-  return res.status(201).json({
-    ok: true,
-    msg: 'register',
-    name,
-    email,
-    password,
-  });
+    await user.save();
+
+    return res.status(201).json({
+      ok: true,
+      msg: 'register',
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      ok: false,
+      msg: 'Please contact the administrator',
+    });
+  }
 };
 
 const loginUser = (req, res = express.reponse) => {
