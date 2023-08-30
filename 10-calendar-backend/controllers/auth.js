a96416612ebd18c07ea96717e419642b161ee061
Michael Javier Mota <michaeljaviermota@gmail.com>
2023-08-30 17:05:30 -0400

    valid login bcrypt

--- 10-calendar-backend/controllers/auth.js
+++ 10-calendar-backend/controllers/auth.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const bcrypt = require('bcryptjs');
+
 const User = require('../models/user.model');
 
 const createUser = async (req, res = express.reponse) => {
@@ -16,6 +18,10 @@ const createUser = async (req, res = express.reponse) => {
 
     //save in database
     user = new User(req.body);
+    //encrypt password
+    const salt = bcrypt.genSaltSync();
+    user.password = bcrypt.hashSync(password, salt);
+
     await user.save();
 
     return res.status(201).json({
@@ -32,13 +38,43 @@ const createUser = async (req, res = express.reponse) => {
   }
 };
 
-const loginUser = (req, res = express.reponse) => {
+const loginUser = async (req, res = express.reponse) => {
   const { email, password } = req.body;
-  res.status(201).json({
-    ok: true,
-    email,
-    password,
-  });
+
+  try {
+    let user = await User.findOne({ email });
+    console.log(user);
+
+    //If not exist
+    if (!user) {
+      return res.status(400).json({
+        ok: false,
+        msg: 'User and password are wrong email',
+      });
+    }
+
+    //confirm password
+    const validPassword = await bcrypt.compareSync(password, user.password);
+
+    if (!validPassword) {
+      return res.status(400).json({
+        ok: false,
+        msg: 'User and password are wrong password',
+      });
+    }
+
+    return res.status(201).json({
+      ok: true,
+      uid: user.id,
+      name: user.name,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      ok: false,
+      msg: 'Please contact the administrator',
+    });
+  }
 };
 
 const reEvaluteToken = (req, res = express.reponse) => {
