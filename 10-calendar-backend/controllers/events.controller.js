66798aad7c169440b388a43d2c336951be8a544a
Michael Javier Mota <michaeljaviermota@gmail.com>
2023-08-31 20:22:01 -0400

    finished usdated

--- 10-calendar-backend/controllers/events.controller.js
+++ 10-calendar-backend/controllers/events.controller.js
@@ -22,8 +22,46 @@ const createEvent = async (req, res = response) => {
   }
 };
 
-const updateEvent = (req, res = response) => {
-  return res.json({ ok: true, msg: 'updateEvent' });
+const updateEvent = async (req, res = response) => {
+  const eventId = req.params.id;
+
+  const uid = req.uid;
+  try {
+    // const events = await Event.findById(eventId).populate('user', 'name ');
+    const event = await Event.findById(eventId);
+    //if not exists
+    if (!event) {
+      return res.status(404).json({
+        ok: false,
+        msg: 'Event not found ',
+      });
+    }
+
+    if (event.user.toString() !== uid) {
+      return res.status(401).json({
+        ok: false,
+        msg: 'do not have permission ',
+      });
+    }
+
+    const eventToUpdate = {
+      ...req.body,
+      user: uid,
+    };
+
+    // event.user = req.uid;
+    const eventUpdated = await Event.findByIdAndUpdate(eventId, eventToUpdate, {
+      new: true,
+    });
+
+    return res.json({ ok: true, event: eventUpdated });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      ok: false,
+      msg: 'Please contact the administrator',
+    });
+  }
 };
 const deleteEvent = (req, res = response) => {
   return res.json({ ok: true, msg: 'deleteEvent' });
