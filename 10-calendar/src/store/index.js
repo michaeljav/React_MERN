0633fb02267c08177efa728548c9e7f68758cc38
Michael Javier Mota <michaeljaviermota@gmail.com>
2023-09-01 13:35:13 -0400

    creating authSlice

--- 10-calendar/src/store/index.js
+++ 10-calendar/src/store/index.js
@@ -1,5 +1,6 @@
 export * from './ui/uiSlice';
 export * from './calendar/calendarSlice';
+export * from './auth/authSlice';
 
 // import befor use create the slice because reducer
 export * from './store';
