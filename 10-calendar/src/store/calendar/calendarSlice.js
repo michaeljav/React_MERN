86c4c8b0904f79c3a127d4c8b39b802b8e4ec772
Michael Javier Mota <michaeljaviermota@gmail.com>
2023-09-02 23:20:11 -0400

    cleaning data calendar redux after logout

--- 10-calendar/src/store/calendar/calendarSlice.js
+++ 10-calendar/src/store/calendar/calendarSlice.js
@@ -54,14 +54,20 @@ export const calendarSlice = createSlice({
         }
       });
     },
+    onLogoutCalendar: (state) => {
+      state.isLoadingEvents = true;
+      state.events = [];
+      state.activeEvent = null;
+    },
   },
 });
 
 // Action creators are generated for each case reducer function
 export const {
-  onSetActiveEvent,
   onAddNewEvent,
-  onUpdateEvent,
   onDeleteEvent,
   onLoadEvent,
+  onLogoutCalendar,
+  onSetActiveEvent,
+  onUpdateEvent,
 } = calendarSlice.actions;
