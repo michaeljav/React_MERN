667d8c5f331b7359e1dd79119cdf6dd5289af7b3
Michael Javier Mota <michaeljaviermota@gmail.com>
2023-09-04 12:40:36 -0400

    PROBLEMA DEPLOY VITE Y .ENV.META

--- 10-calendar/src/helpers/getEnvVariables.js
+++ 10-calendar/src/helpers/getEnvVariables.js
@@ -1,9 +1,19 @@
+//PARA DESPLEGAR PROBLEMA CON  .ENV.META Y VITE
+
+// export const getEnvVariables = () => {
+//   ////este es un error de vite
+//   ////debo de comentarlo y ponerlo  LA VARIABLES DE ENTORNO DE MANERA MANUAL
+//   //import.meta.env;
+//   return {
+//     //...import.meta.env,
+//     VITE_API_URL: import.meta.env.VITE_API_URL,
+//   };
+// };
+
+//ESTO SOLO FUNCIONA EN DESARROLLO PARA DEPLOY DEBO USAR CODIGO DE ARRIBA
 export const getEnvVariables = () => {
-  ////este es un error de vite
-  ////debo de comentarlo y ponerlo  LA VARIABLES DE ENTORNO DE MANERA MANUAL
-  //import.meta.env;
+  import.meta.env;
   return {
-    //...import.meta.env,
-    VITE_API_URL: import.meta.env.VITE_API_URL,
+    ...import.meta.env,
   };
 };
