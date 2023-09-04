39ee1fd7208347ad2b8f73dea3d97d39a3928ada
Michael Javier Mota <michaeljaviermota@gmail.com>
2023-09-04 14:09:49 -0400

    devolvi los cambios hashrouter para usar metodo 2

--- 10-calendar/src/CalendarApp.jsx
+++ 10-calendar/src/CalendarApp.jsx
@@ -7,11 +7,11 @@ export const CalendarApp = () => {
   return (
     <Provider store={store}>
       {/*METODO 1  esto es para cuando refresco y busco una ruta que  mi router no la soporta porque mi router comienza a funcionar desde el index ya que es una SPA SINGLE PAGE APPLICATION */}
-      {/* <BrowserRouter> */}
-      <HashRouter>
+      <BrowserRouter>
+        {/* <HashRouter> */}
         <AppRouter />
-      </HashRouter>
-      {/* </BrowserRouter> */}
+        {/* </HashRouter> */}
+      </BrowserRouter>
     </Provider>
   );
 };
