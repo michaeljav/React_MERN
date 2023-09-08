95f00616348116f7ab71ad9ba0ad244a85be2e39
Michael Javier Mota <michaeljaviermota@gmail.com>
2023-09-08 11:34:33 -0400

    calendarSlice jest test

--- 10-calendar/tests/store/calendar/calendarSlice.test.js
+++ 10-calendar/tests/store/calendar/calendarSlice.test.js
@@ -1,10 +1,14 @@
 import {
   calendarSlice,
   onAddNewEvent,
+  onDeleteEvent,
+  onLoadEvent,
+  onLogoutCalendar,
   onSetActiveEvent,
   onUpdateEvent,
 } from '../../../src/store/calendar/calendarSlice';
 import {
+  calendarWithActiveEventsState,
   calendarWithEventsState,
   events,
   initialState,
@@ -61,5 +65,32 @@ describe('Pruebas en calendarSlice', () => {
     // console.log(state);
 
     expect(state.events).toContain(updatedEvent);
+  });
+
+  test('onDeleteEvent debe de borrar el evento activo ', () => {
+    const state = calendarSlice.reducer(
+      calendarWithActiveEventsState,
+      onDeleteEvent()
+    );
+    expect(state.activeEvent).toBe(null);
+    expect(state.events).not.toContain(events[0]); // el evento activo el que puede en calendarWithActiveEventState fue state[0] manualmente
+  });
+
+  test('onLoadEvent debe de establecer los eventos ', () => {
+    const state = calendarSlice.reducer(initialState, onLoadEvent(events));
+    expect(state.isLoadingEvents).toBeFalsy(); //
+    expect(state.events).toEqual(events);
+
+    //no se dupliquen los eventos
+    const newstate = calendarSlice.reducer(state, onLoadEvent(events));
+    expect(state.events.length).toBe(events.length);
+  });
+
+  test('onLogoutCalendar debe de limpiar el estado ', () => {
+    const state = calendarSlice.reducer(
+      calendarWithActiveEventsState,
+      onLogoutCalendar()
+    );
+    expect(state).toEqual(initialState);
   });
 });
