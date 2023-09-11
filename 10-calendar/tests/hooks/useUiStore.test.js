405a697da42a84af7487a18ce3a2dbd59162609a
Michael Javier Mota <michaeljaviermota@gmail.com>
2023-09-11 17:18:28 -0400

    modal useUiSotore

--- 10-calendar/tests/hooks/useUiStore.test.js
+++ 10-calendar/tests/hooks/useUiStore.test.js
@@ -3,6 +3,7 @@ import { useUiStore } from '../../src/hooks';
 import { Provider } from 'react-redux';
 import { store, uiSlice } from '../../src/store';
 import { configureStore } from '@reduxjs/toolkit';
+import { act } from 'react-dom/test-utils';
 
 const getMockStore = (initialState) => {
   return configureStore({
@@ -31,5 +32,68 @@ describe('Pruebas en useUiStore', () => {
       closeDateModal: expect.any(Function),
       toggleDateModal: expect.any(Function),
     });
+  });
+
+  test('openDateModal debe de colocar tru en el isDateModalOpen', () => {
+    const mockStore = getMockStore({ isDateModalOpen: false });
+    const { result } = renderHook(() => useUiStore(), {
+      wrapper: ({ children }) => (
+        <Provider store={mockStore}>{children}</Provider>
+      ),
+    });
+    //este valor se mantiene en false porque se codej por valor y no por refferencia
+    // const { isDateModalOpen, openDateModal } = result.current;
+    const { openDateModal } = result.current;
+
+    act(() => {
+      openDateModal();
+    });
+
+    //este valor se mantiene en false porque se codej por valor y no por refferencia
+    // console.log({ result: result.current, isDateModalOpen });
+    expect(result.current.isDateModalOpen).toBeTruthy();
+  });
+
+  test('closeDateModal debe de colocar false en  isDateModeOpen', () => {
+    const mockStore = getMockStore({ isDateModalOpen: false });
+    const { result } = renderHook(() => useUiStore(), {
+      wrapper: ({ children }) => (
+        <Provider store={mockStore}>{children}</Provider>
+      ),
+    });
+    //este valor se mantiene en false porque se codej por valor y no por refferencia
+    // const { isDateModalOpen, openDateModal } = result.current;
+    const { openDateModal } = result.current;
+
+    act(() => {
+      openDateModal();
+    });
+
+    expect(result.current.isDateModalOpen).toBeTruthy();
+  });
+
+  test('toggleDateModal debe de cambiar el estado respectivamente', () => {
+    const mockStore = getMockStore({ isDateModalOpen: true });
+    const { result } = renderHook(() => useUiStore(), {
+      wrapper: ({ children }) => (
+        <Provider store={mockStore}>{children}</Provider>
+      ),
+    });
+    //este valor se mantiene en false porque se codej por valor y no por refferencia
+    // const { isDateModalOpen, openDateModal } = result.current;
+
+    // const { toggleDateModal } = result.current;
+
+    act(() => {
+      result.current.toggleDateModal();
+    });
+
+    expect(result.current.isDateModalOpen).toBeFalsy();
+
+    act(() => {
+      result.current.toggleDateModal();
+    });
+
+    expect(result.current.isDateModalOpen).toBeTruthy();
   });
 });
